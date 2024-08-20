72512371a9769b6562d6ad35ee730b83a3cecb3f
Aliakbar Boistov <aliakbarboistov@Aliakbars-MacBook-Pro.local>
2024-08-20 17:10:16 +0500

    added databsase ---

--- database.py
+++ database.py
@@ -9,10 +9,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Get the database URL from the environment variable
-DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASE_URL = "sqlite:///./sqlite.db"
 
 # Create the SQLAlchemy engine
-engine = sqlalchemy_package.create_engine(DATABASE_URL)
+engine = sqlalchemy_package.create_engine(DATABASE_URL,connect_args={"check_same_thread": True})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
